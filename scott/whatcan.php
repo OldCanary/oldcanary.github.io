7c5bac2ba9b477430e7c0a4997a9868088cf0f1d
OldCanary <oldcanary15@gmail.com>
2016-03-27 10:54:37 -0500

    Removed extraneous info from Brandy's site

---
+++ scott/whatcan.php
@@ -0,0 +1,161 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta name="resource-type" content="document"/>
+<meta name="Generator" content="sitehatc Tag Generator"/>
+<meta name="revisit-after" content="10"/>
+<meta name="classification" content="Design"/>
+<meta name="description" content="My name is Scott and I'm Chemically Injured"/>
+<meta name="keywords" content="MCS, TILT, Chemically, Injured, Illinois"/>
+<meta name="robots" content="ALL"/>
+<meta name="distribution" content="Local"/>
+<meta name="rating" content="General"/>
+<meta name="copyright" content="2016"/>
+<meta name="author" content="brandyoldfashion"/>
+<meta http-equiv="Content-Language" content="English"/>
+<title>Chemically Injured</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+<link href="css/styles.css" rel="stylesheet" type="text/css"/>
+<script type='text/javascript'>
+<!--
+	home = new Image();
+	home.src = "images/home.png";
+	homeOver = new Image();
+	homeOver.src = "images/homeOver.png";
+    
+    about = new Image();
+	about.src = "images/about.png";
+	aboutOver = new Image();
+	aboutOver.src = "images/aboutOver.png";
+    
+    linking = new Image();
+	linking.src = "images/linking.png";
+	linkingOver = new Image();
+	linkingOver.src = "images/linkingOver.png";
+    
+    contact = new Image();
+	contact.src = "images/contact.png";
+	contactOver = new Image();
+	contactOver.src = "images/contactOver.png";	
+    
+    what = new Image();
+	what.src = "images/what.png";
+	whatOver = new Image();
+	whatOver.src = "images/whatOver.png";
+    
+    pictures = new Image();
+	pictures.src = "images/pictures.png";
+	picturesOver = new Image();
+	picturesOver.src = "images/picturesOver.png";
+    
+    can = new Image();
+	can.src = "images/can.png";
+	canOver = new Image();
+	canOver.src = "images/canOver.png";
+// -->
+</script>
+</head>
+<body>
+    <div id="header">    
+        <div style="float: left; width: 551px; height: 118px">
+            <a href="../index.html"><img src="images/headera.png" border="0"></a></div>
+        <div style="float: left; width: 449px; height: 118px;">             
+            <div style="float: left; width: 123px;"><a href="index.php"
+					onMouseOver="document.homeDisplay.src=homeOver.src"
+					onMouseOut="document.homeDisplay.src=home.src"><img
+						name="homeDisplay"
+						align="left"
+						src="images/home.png"
+						alt="Home"
+						border="0" /></a></div>
+                        
+            <div style="float: left; width: 129px;"><a href="about.php"
+					onMouseOver="document.aboutDisplay.src=aboutOver.src"
+					onMouseOut="document.aboutDisplay.src=about.src"><img
+						name="aboutDisplay"
+						align="left"
+						src="images/about.png"
+						alt="About MCS"
+						border="0" /></a></div>
+                        
+            <div style="float: left; width: 92px;"><a href="links.php"
+					onMouseOver="document.linkingDisplay.src=linkingOver.src"
+					onMouseOut="document.linkingDisplay.src=linking.src"><img
+						name="linkingDisplay"
+						align="left"
+						src="images/linking.png"
+						alt="Links"
+						border="0" /></a></div>
+                        
+            <div style="float: left; width: 105px;"><a href="contact.php"
+					onMouseOver="document.contactDisplay.src=contactOver.src"
+					onMouseOut="document.contactDisplay.src=contact.src"><img
+						name="contactDisplay"
+						align="left"
+						src="images/contact.png"
+						alt="Contact"
+						border="0" /></a></div>        
+        </div>
+    </div>
+    <div id="yellowline"></div>
+    <div id="homecontent">
+        <div style="padding: 40px 10px 30px 10px;">
+            <div style="float: left; width: 300px; margin-right: 30px; background-color: #f1ece5;">
+                <div style="text-align: left; padding: 20px;">
+            
+                    <a  href="http://www.cancer.gov/publications/dictionaries/cancer-terms?cdrid=687391" target="_blank">Toxicants that cause cancer</a><br /><br />
+                    
+                    <a href="http://www.ei-resource.org/illness-information/environmental-illnesses/multiple-chemical-sensitivity/" target="_blank">Overview of MCS</a><br /><br />
+                    
+                    <a href="http://peggymunson.com/mcs/myths.html" target="_blank">Myths and Facts about MCS</a><br /><br />
+                    
+                    <a href="https://www.flickr.com/photos/41565963@N04/" target="_blank">My Flcker page</a><br /><br />
+                    
+                    <a href="pictures.php" target="_blank">A picture is worth 1,000 words</a><br /><br />
+                    
+                    <a href="http://www.butyoudontlooksick.com/articles/written-by-christine/the-spoon-theory/" target="_blank">What is the "Spoon Theory"?</a><br /><br />
+                    
+                    
+                    
+                </div>
+            </div>
+            <div style="float: left; width: 650px;">
+                <img src="images/whatcanido.png"/><br /><br />
+                
+                
+                
+            
+            
+            
+                 
+            <br /><br /><br />
+            </div>
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+    <div id="yellowfooter">
+        <div style="width: 1000px; margin-right: auto; margin-left: auto;">
+            <div style="text-align: left; padding; 25px 0 0 70px;">
+                <br /><br />
+                &nbsp;&nbsp;&nbsp;&nbsp;<a href="index.php" class="footer">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="about.php" class="footer">About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="links.php" class="footer">Links</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="faq.php" class="footer">FAQs</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="resources.php" class="footer">Resources</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="pictures.php" class="footer">Pictures</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="whatcan.php" class="footer">What Can I Do?</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <a href="contact.php" class="footer">Contact</a>            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
